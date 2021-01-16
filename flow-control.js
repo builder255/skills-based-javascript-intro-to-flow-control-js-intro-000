43488733ed46e165f658e83b7569fad282952eda
Rich D <musiclike@hotmail.com>
2021-01-16 17:39:33 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,8 @@
-function basicTeenager(flow-control basicTeenager should return "You are a teenager!" if the age is between 13-19:) {
-  if (If the age is between 13 and 19 it should return "You are a teenager!") {
-    // condition is false hence code is not executed
-} else if (Otherwise, the function should return "You are not a teenager") {
-  // execute this code if `conditionToTest1`statement is falsey AND `conditionToTest2` is truthy
+function basicTeenager(age) {
+  if (age >= 13 && age <= 19) {
+return "You are a teenager!";
+} else {return "You are not a teenager";}
+}
 }
 
 }
